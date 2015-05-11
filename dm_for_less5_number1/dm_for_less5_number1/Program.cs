4bf763c88f1170cfef1ce516065b11e2f4b887b3
loglucho <luchezar9507>
2015-05-11 19:24:47 +0300

    bavla

---
+++ dm_for_less5_number1/dm_for_less5_number1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dm_for_less5_number1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("napishete EDNA cifra   ot 0 do cifra 9");
+            int a;
+            a = int.Parse(Console.ReadLine());
+            if (a == 0)
+            {
+                Console.WriteLine("Nula/Zero");
+            }
+            else if (a == 1)
+            {
+                Console.WriteLine("Edno/One");
+            }
+            else if (a == 2)
+            {
+                Console.WriteLine("Dve/Two");
+            }
+            else if (a == 3)
+            {
+                Console.WriteLine("Tri/Tree");
+            }
+            else if (a == 4)
+            {
+                Console.WriteLine("Chetri/Four");
+            }
+            else if (a == 5)
+            {
+                Console.WriteLine("Pet/Five");
+            }
+            else if (a == 6)
+            {
+                Console.WriteLine("Shest/Six");
+            }
+            else if (a == 7)
+            {
+                Console.WriteLine("Sedem/Seven");
+            }
+            else if (a == 8)
+            {
+                Console.WriteLine("Osem/Eight");
+            }
+            else if (a == 9)
+            {
+                Console.WriteLine("Devet/Nine");
+            }
+            else
+            {
+                Console.WriteLine("NE NAPRAI TVA DE TREA / YOU DID NOT DO THAT I WAS SUPPOSED TO ");
+            }
+        }
+    }
+}
---
+++ dm_for_less5_number2/dm_for_less5_number2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dm_for_less5_number2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" napisete si tochkite ot 1 do 9");
+            int a ;
+            a = int.Parse(Console.ReadLine());
+            switch (a)
+            {
+                case 1 :
+                case 2 :
+                case 3: Console.WriteLine(" umnojeno po 5 e =  "); Console.WriteLine(a = a * 5); break;
+                case 4 :
+                case 5 :
+                case 6: Console.WriteLine("umnojeno po 10 = "); Console.WriteLine(a = a * 10); break;
+                case 7 :
+                case 8 :
+                case 9: Console.WriteLine("umnojeno po 50 e = "); Console.WriteLine(a = a * 50); break;
+                default: Console.WriteLine(" NE SI NAPISAL TVA DE TREBA "); break;
+            }
+
+
+
+        }
+    }
+}
